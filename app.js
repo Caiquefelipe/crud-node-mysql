3db51b5a1150f42ef601da0be53cdf5fc7ab1299
caique <felipecaique48@gmail.com>
2025-12-10 14:56:00 -0300

    Criando rota de cadastro

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //importar módulo express
 const express = require('express');
 
+
+//importar modulo fileupload
+const fileupload = require('express-fileupload');
+
 //Importar módulo express-handlebars
 const { engine } = require('express-handlebars');
 
@@ -24,19 +28,63 @@ conexão.connect((erro) => {
 
 const app = express();
 
+//habilitar o fileupload
+app.use(fileupload());
+
 //Adicionar Bootstrap
 app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
+//adicionar CSS
+app.use('/css', express.static('./css'))
 
 //configuração do express-handlebars
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+
+//Manipulação de dados por rotas
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
 //Rota Principal
 app.get('/', (req, res) => {
-    res.render('formulario');
+    //SQL para selecionar os produtos
+    let sql = 'SELECT * FROM produtos';
+
+    //Executar o SQL
+    conexão.query(sql, (erro, retorno) => {
+        if (erro) throw erro;
+
+        //Renderizar a página com os produtos
+        res.render('formulario', { produtos: retorno })
+    })
 });
 
+
+//Rota de cadastro
+
+app.post('/cadastrar', (req, res) => {
+    //obter os dados que serão utilizados para o cadastro
+    let nome = req.body.nome;
+    let valor = req.body.valor;
+    let imagem = req.files.imagem.name;
+
+
+    //SQL
+    let sql = `INSERT INTO produtos (nome, valor, imagem) VALUES ('${nome}', ${valor}, '${imagem}')`;
+
+    //Executar o SQL
+    conexão.query(sql, (erro, retorno) => {
+        if (erro) throw erro;
+
+        //caso ocorra o cadastro
+        req.files.imagem.mv(__dirname + '/imagens/' + req.files.imagem.name)
+        console.log(retorno)
+    })
+
+    res.redirect('/')
+})
 
 //Servidor
 app.listen(8080);
